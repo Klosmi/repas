378e146a6d5bd82b0f04051050555cde7e85e72b
linda karmouche <lindakarmouche@yahoo.fr>
2021-03-16 23:19:38 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all if Rails.env.development?
+
+user= User.new(email:"lindak12@yahoo.fr", password:"password")
+user.save
+
+Event.create!(
+        title: " Wedding Escale Borely ",
+        date: 20210309,
+        number_guest: 208,
+        address: "79/81 Avenue de la Pointe Rouge, 13008 Marseille",
+        user_id: user.id
+        )
+
+Event.create!(
+        title: " Paul’s 60th Birthday ",
+        date: 20210602,
+        number_guest: 62,
+        address: "122 rue de l'évêcher, 13002 Marseille",
+        user_id: user.id
+        )
+
+ Event.create!(
+        title: " Wedding  Andrea Nocelli ",
+         date: 20210202,
+         number_guest: 78,
+         address: "164 Boulevard de Plombières, 13014 Marseille",
+         user_id: user.id
+         )
+
+Event.create!(
+         title: "Baby Shower Mr and Mrs Willi",
+         date: 20210801,
+         number_guest: 142,
+         address: "19 Boulevard Louis Salvator, 13006 Marseille",
+         user_id: user.id
+         )
+
+
+ Event.create!(
+         title: "Seminar Company Brooms",
+         date: 20201212,
+         number_guest: 82,
+         address: "117 Traverse de la Montre, 13011 Marseille",
+         user_id: user.id
+         )
+
+ Event.create!(
+         title: "Baptism Adam Junior",
+         date: 20201203,
+         number_guest: 45,
+         address: "72 Chemin des Bessons, 13014 Marseille",
+         user_id: user.id
+         )
