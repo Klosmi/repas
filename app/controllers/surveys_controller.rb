d661223f9d4f0381687b37699fa1a60076e0d234
lindaounissi <linda.ounissi020712@gmail.com>
2021-03-18 15:56:58 +0100

    form survey and view congratulation

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,37 +1,32 @@
 class SurveysController < ApplicationController
-
-
-
-def create
+  def create
   @event = Event.find(params[:event_id])
+  @guest = Guest.find(params[:guest_id])
   @survey = Survey.new(survey_params)
-  @survey.event = @event
-  if @survey.save
-  else
-    render "events/show"
+  @surveys.guest.event = @survey
+    if @survey.save
+    else
+      render "events/show"
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:event_id])
+    @guest = Guest.find(params[:guest_id])
+    @survey = Survey.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:event_id])
+    @survey = Survey.update(survey_params)
+    redirect_to event_confirmation_path(@event)
+  end
+  def confirmation
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:nut, :peanut, :shellfish, :egg, :fish, :soy, :celery, :sesame_seed, :milk, :sulfite, :mustard, :gluten, :salt, :sugar, :hallal, :casher, :vegan, :vegetarian, :comment)
   end
 end
-
-
-def edit
-    @survey = Survey.find(params[:id])
-end
-def update
-    @survey = Survey.update(survey_params)
-    @guest = Guest.find(params[:guest_id])
-    @survey.guest = @guest
-    redirect_to survey_path(@survey)
-end
-
-
-
-#  private
-
-#   def restaurant_params
-#     params.require(:survey).permit(:nut, :peanut, :shellfish)
-#   end
-# end
-
-
-
-
