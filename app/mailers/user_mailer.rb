652a303b27087407519bfc59b1745be5c1a9ebc6
linda karmouche <lindakarmouche@yahoo.fr>
2021-03-23 22:52:28 +0100

    smtpmailgun-ok

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
   #
   def welcome
     @user = params[:user] # Instance variable => available in view
-    mail(to: @user.email, subject: 'Welcome to ucaneat')
+    mail(to: @user.email, subject: 'Welcome to CustomFood')
     # This will render a view in `app/views/user_mailer`!
   end
 end
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,19 @@
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   address: "smtp.gmail.com",
+#   port: 587,
+#   domain: 'gmail.com',
+#   user_name: ENV['GMAIL_ADDRESS'],
+#   password: ENV['GMAIL_APP_PASSWORD'],
+#   authentication: :login,
+#   enable_starttls_auto: true
+# }
 
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'sandboxbe11a479d2ae419c912170012a67562c.mailgun.org',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
